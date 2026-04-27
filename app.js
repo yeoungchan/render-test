7376d1f50b0b2aadd91a3119443287ca8188a101
yeoungchan <163278327+yeoungchan@users.noreply.github.com>
2026-04-27 17:23:53 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
 http.createServer((req,res)=>{
-  res.writeHead(200,{
-    'Content-Type':'text/html; charset=utf-8'
-  });
-  res.end('<h1>안녕하세요</h1>');
+
+ if(req.url === '/api'){
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify({
+       msg:"API 성공",
+       amount:10000
+    }));
+ }
+
+ else{
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
+    res.end('<h1>메인 홈페이지</h1>');
+ }
+
 }).listen(process.env.PORT || 3000);
