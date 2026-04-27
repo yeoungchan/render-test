7339f73be8563e20b5ea01a04913eac803260843
yeoungchan <163278327+yeoungchan@users.noreply.github.com>
2026-04-27 17:13:44 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const http = require('http');
 
 http.createServer((req,res)=>{
  res.writeHead(200,{'Content-Type':'text/html'});
- res.end('<h1>Hello Render!</h1>');
+ res.end('<h1>내 첫 클라우드 서버</h1>');
 }).listen(process.env.PORT || 3000);
