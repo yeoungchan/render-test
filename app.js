62e020d2c717529dd0cdd1722470d674b29239a9
yeoungchan <163278327+yeoungchan@users.noreply.github.com>
2026-05-06 13:48:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
 const express = require("express");
 const path = require("path");
+const mysql = require("mysql2");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// MySQL 연결
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("MySQL 연결 실패:", err);
+    return;
+  }
+
+  console.log("MySQL 연결 성공!");
+});
+
 app.use(express.static(__dirname));
 
-app.get("/", (req,res)=>{
- res.sendFile(path.join(__dirname,"index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/mall",(req,res)=>{
- res.sendFile(path.join(__dirname,"mall.html"));
+app.get("/mall", (req, res) => {
+  res.sendFile(path.join(__dirname, "mall.html"));
 });
 
-app.listen(PORT);
+// 테스트 API
+app.get("/shops", (req, res) => {
+  db.query("SHOW TABLES", (err, results) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json(results);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`서버 실행중: ${PORT}`);
+});
